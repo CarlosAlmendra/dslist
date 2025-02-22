e1c488550f7a30cb68520619c7a9721719777364
Carlos <almendracarloshenrique@gmail.com>
2025-02-22 17:22:43 -0300

    add projection

--- src/main/java/com/gameproject/dslist/dto/GameMinDTO.java
+++ src/main/java/com/gameproject/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.gameproject.dslist.dto;
 
 import com.gameproject.dslist.entities.Game;
+import com.gameproject.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -18,6 +19,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getGameYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
